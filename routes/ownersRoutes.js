0c9e90582e9cef5ebff6821674032975aaecae8d
show source control <view source control>
2025-04-02 22:38:06 +0530

    development and production set-up

--- routes/ownersRoutes.js
+++ routes/ownersRoutes.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel= require('../models/owners-model');
 
-router.get('/', (req, res) => {
-    res.send('owners route is working!');
+if(process.env.NODE_ENV=="development"){
+   
+    router.post("/create",async (req, res) => {
+        
+      let owners=await ownerModel.find();
+      if(owners.length>0){
+            return res
+            .status(503)
+            .send("owner already exists");
+      }
+      
+      let { fullname, email, password } = req.body;
+
+      let createdOwner=await ownerModel.create({
+        fullname,
+        email,
+        password,
+      })
+      res.status(201).send(createdOwner);
+    });
+}
+
+router.get('/', (req, res) =>  {
+    res.send('owners route is working hahah!');
 });
+
+
+console.log(process.env.NODE_ENV);
 
 module.exports = router;
